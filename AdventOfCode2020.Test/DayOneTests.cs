5763c77ab0f1c5b605619bb625d868b5238170f6
Jayarrowz <arrowzflame321@gmail.com>
2020-12-02 00:40:58 +0000

    AOC Q1

---
+++ AdventOfCode2020.Test/DayOneTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode2020.Test
+{
+    public class DayOneTests
+    {
+        private static readonly string FilePath = "./TestData/DayOne.txt";
+
+        [Theory]
+        [InlineData(new int[] { 1721, 979, 366, 299, 675, 1456 }, 514579)]
+        public void TestExampleOne(int[] data, int expectedProduct)
+        {
+            DayOne.GetListProduct(data, 2020).Should().Be(expectedProduct);
+        } 
+        
+        [Theory]
+        [InlineData(new int[] { 1721, 979, 366, 299, 675, 1456 }, 241861950)]
+        public void TestExampleTwo(int[] data, int expectedProduct)
+        {
+            DayOne.GetListTriProduct(data, 2020).Should().Be(expectedProduct);
+        }
+
+        [Fact]
+        public void TestPartOne()
+        {
+            var expenses = File.ReadAllLines(FilePath).Select(int.Parse).ToArray();
+            var product = DayOne.GetListProduct(expenses, 2020);
+            Console.WriteLine(product);
+        }
+
+        [Fact]
+        public void TestPartTwo()
+        {
+            var expenses = File.ReadAllLines(FilePath).Select(int.Parse).ToArray();
+            var product = DayOne.GetListTriProduct(expenses, 2020);
+            Console.WriteLine(product);
+        }
+    }
+}
---
+++ AdventOfCode2020/DayOne.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class DayOne
+    {
+        public static int GetListProduct(int[] expenses, int expectedSum)
+        {
+            return expenses.Select(expense => expense * expenses.FirstOrDefault(expensesB => expensesB + expense == expectedSum))
+                .Where(expenseProduct => expenseProduct > 0)
+                .FirstOrDefault();
+        }
+
+        public static int GetListTriProduct(int[] expenses, int expectedSum)
+        {
+            return expenses.SelectMany(expense => expenses.Select(expenseB =>
+            {
+                var finalNumber = expenses.FirstOrDefault(expensesC => expense + expenseB + expensesC == expectedSum);
+                var finalNumberIsValid = finalNumber > 0;
+                return finalNumberIsValid ? expense * expenseB * finalNumber: 0;
+            }))
+                .Where(expenseProduct => expenseProduct > 0)
+                .FirstOrDefault();
+        }
+    }
+}
