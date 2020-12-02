be983b7ff4f6757ad45157ca94ea02b941a0ec2e
Jayarrowz <arrowzflame321@gmail.com>
2020-12-02 02:28:32 +0000

    Do not count same integer twice

--- AdventOfCode2020/DayOne.cs
+++ AdventOfCode2020/DayOne.cs
@@ -6,16 +6,16 @@ namespace AdventOfCode2020
     {
         public static int GetListBiProduct(int[] expenses, int expectedSum)
         {
-            return expenses.Select(expense => expense * expenses.FirstOrDefault(expensesB => expensesB + expense == expectedSum))
+            return expenses.Select(expense => expense * expenses.Except(new int[] { expense }).FirstOrDefault(expensesB => expensesB + expense == expectedSum))
                 .Where(expenseProduct => expenseProduct > 0)
                 .FirstOrDefault();
         }
 
         public static int GetListTriProduct(int[] expenses, int expectedSum)
         {
-            return expenses.SelectMany(expense => expenses.Select(expenseB =>
+            return expenses.SelectMany(expense => expenses.Except(new int[] { expense }).Select(expenseB =>
             {
-                var finalNumber = expenses.FirstOrDefault(expensesC => expense + expenseB + expensesC == expectedSum);
+                var finalNumber = expenses.Except(new int[] { expense, expenseB }).FirstOrDefault(expensesC => expense + expenseB + expensesC == expectedSum);
                 var finalNumberIsValid = finalNumber > 0;
                 return finalNumberIsValid ? expense * expenseB * finalNumber: 0;
             }))
