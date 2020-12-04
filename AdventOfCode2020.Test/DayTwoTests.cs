82b5b82395b7d7ce6835216b975bfb7506b3ceed
Jayarrowz <arrowzflame321@gmail.com>
2020-12-04 14:42:22 +0000

    Add Day 2

---
+++ AdventOfCode2020.Test/DayTwoTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2020.DayTwo;
+
+namespace AdventOfCode2020.Test
+{
+    public class DayTwoTests
+    {
+        private readonly ITestOutputHelper _output;
+        private static readonly List<PasswordInput> _exampleData = new List<PasswordInput>
+        {
+                new PasswordInput('a', "abcde", 1, 3),
+                new PasswordInput('b', "cdefg", 1, 3),
+                new PasswordInput('c', "ccccccccc", 2, 9)
+        };
+
+        public DayTwoTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private static readonly string FilePath = "./TestData/DayTwo.txt";
+
+        [Fact]
+        public void TestExampleOne()
+        {
+            GetValidPasswords(_exampleData).Should().Be(2);
+        }
+
+        [Fact]
+        public void TestExampleTwo()
+        {
+            GetValidPasswordsPartTwo(_exampleData).Should().Be(1);
+        }
+
+        [Fact]
+        public void TestQuestionOne()
+        {
+            var passwordInputs = GetPasswordInputFromFile();
+            _output.WriteLine("Day 2 Q1: {0}", GetValidPasswords(passwordInputs));
+        }
+
+        [Fact]
+        public void TestQuestionTwo()
+        {
+            var passwordInputs = GetPasswordInputFromFile();
+            _output.WriteLine("Day 2 Q2: {0}", GetValidPasswordsPartTwo(passwordInputs));
+        }
+
+
+        private List<PasswordInput> GetPasswordInputFromFile()
+        {
+            return File.ReadAllLines(FilePath).Select(t =>
+            {
+                var data = t.Replace(":", string.Empty).Split(" ");
+                var ranges = data[0].Split("-");
+                var minRange = int.Parse(ranges[0]);
+                var maxRange = int.Parse(ranges[1]);
+                var letter = data[1][0];
+                var password = data[2];
+                return new PasswordInput(letter, password, minRange, maxRange);
+            }).ToList();
+        }
+    }
+}
---
+++ AdventOfCode2020/DayTwo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class DayTwo
+    {
+        public static int GetValidPasswords(List<PasswordInput> passwordInput)
+        {
+            return passwordInput.Count(IsValid);
+        }
+
+        public static int GetValidPasswordsPartTwo(List<PasswordInput> passwordInput)
+        {
+            return passwordInput.Count(IsValidPartTwo);
+        }
+
+        private static bool IsValidPartTwo(PasswordInput input)
+        {
+            return input.Password.Where((c, index) => c == input.Letter &&
+            (index + 1 == input.MinRange || index + 1 == input.MaxRange))
+                .Count() == 1;
+        }
+
+        private static bool IsValid(PasswordInput input)
+        {
+            var letterOccurances = input.Password.Count(pwChar => pwChar.Equals(input.Letter));
+            return letterOccurances >= input.MinRange && letterOccurances <= input.MaxRange;
+        }
+
+        public class PasswordInput
+        {
+            private readonly int _minRange;
+            private readonly int _maxRange;
+            private readonly char _letter;
+            private readonly string _password;
+
+            public PasswordInput(char letter, string password, int minRange, int maxRange)
+            {
+                _letter = letter;
+                _password = password;
+                _minRange = minRange;
+                _maxRange = maxRange;
+            }
+
+            public int MaxRange { get => _maxRange; }
+            public int MinRange { get => _minRange; }
+            public string Password { get => _password; }
+            public char Letter { get => _letter; }
+        }
+    }
+}
